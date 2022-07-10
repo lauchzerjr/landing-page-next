7ed8bf26774a11c900e623d7fccdc515808b182d
Adalberto Junior <adljunior@rd.com.br>
2022-07-10 19:08:10 -0300

    Finished the project

---
+++ src/components/SectionModules/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+import Heading from 'components/Heading'
+import Container from 'components/Container'
+import CardModule from 'components/CardModule'
+
+import * as S from './styles'
+import { SectionModulesProps } from 'types/api'
+
+const SectionModules = ({ modules, title }: SectionModulesProps) => (
+  <Container>
+    <Heading reverseColor>{title}</Heading>
+
+    <S.Content>
+      {modules.map(({ title, subtitle, description, id }) => (
+        <CardModule key={id} title={title} subTitle={subtitle}>
+          <div dangerouslySetInnerHTML={{ __html: description }} />
+        </CardModule>
+      ))}
+    </S.Content>
+  </Container>
+)
+
+export default SectionModules
